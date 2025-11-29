e3d7dbb5e19a984ffb3c6654877aa5c0c745ab0a
trytocode <annguyendang.17.07.2002@gmail.com>
2025-11-29 23:42:45 +0700

    nop bai tap

--- module5/bai_lam_them/bai_1.js
+++ module5/bai_lam_them/bai_1.js
@@ -6,8 +6,8 @@ const checkIsPrime = ((num) => {
         return true;
     }
     if (num > 2) {
-        for (let i = 2; i <= num; i++) {
-            if (i < num && num % i === 0) {
+        for (let i = 2; i < num; i++) {
+            if (num % i === 0) {
                 return false;
             }
         }
